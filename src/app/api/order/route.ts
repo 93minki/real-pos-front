015876a2062778e573fb45155fbddde561814748
minki <alsrl0023@naver.com>
2024-08-22 15:47:25 +0900

    refactor: 오늘 날짜 주문만 가져오도록 수정

--- src/app/api/order/route.ts
+++ src/app/api/order/route.ts
@@ -2,10 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const response = await fetch("http://localhost:8080/order", {
+    const response = await fetch("http://localhost:8080/order/today", {
       method: "GET",
     });
     const data = await response.json();
+    console.log("data", data);
     return NextResponse.json({ success: true, data });
   } catch (error) {
     console.error("GET request error:", error);
