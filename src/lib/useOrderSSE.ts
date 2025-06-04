135203adbac8981a3a8bd3955542dd85c894ef7f
minki <alsrl0023@naver.com>
2025-06-04 15:35:08 +0900

    feat: 사용자 데이터 기반 SSE 연결 로직 개선

--- src/lib/useOrderSSE.ts
+++ src/lib/useOrderSSE.ts
@@ -1,4 +1,6 @@
-import { useQueryClient } from "@tanstack/react-query";
+import { fetchWithAuth } from "@/lib/fetchWithAuth";
+import { UserResponseType } from "@/lib/UserResponseType";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useEffect, useRef } from "react";
 
 export function useOrderSSE() {
@@ -6,7 +8,21 @@ export function useOrderSSE() {
   const abortControllerRef = useRef<AbortController | null>(null);
   const reconnectTimeoutRef = useRef<NodeJS.Timeout | null>(null);
 
+  const { data: userData } = useQuery({
+    queryKey: ["user"],
+    queryFn: async () => {
+      const fetchData = await fetchWithAuth("/api/user");
+      const response: UserResponseType = await fetchData.json();
+      return response;
+    },
+  });
+
   useEffect(() => {
+    // user.id가 없으면 SSE 연결을 시도하지 않음
+    if (!userData?.id) {
+      return;
+    }
+
     const apiUrl = process.env.NEXT_PUBLIC_API_URL;
     let buffer = "";
     const connectSSE = async () => {
@@ -14,15 +30,17 @@ export function useOrderSSE() {
         abortControllerRef.current?.abort();
         abortControllerRef.current = new AbortController();
 
-        const response = await fetch(`${apiUrl}/orders/sse`, {
-          method: "GET",
-          headers: {
-            Accept: "text/event-stream",
-            "Cache-Control": "no-cache",
-          },
-          credentials: "include",
-          signal: abortControllerRef.current.signal,
-        });
+        const response = await fetch(
+          `${apiUrl}/orders/sse?userId=${userData.id}`,
+          {
+            method: "GET",
+            headers: {
+              Accept: "text/event-stream",
+              "Cache-Control": "no-cache",
+            },
+            signal: abortControllerRef.current.signal,
+          }
+        );
 
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
@@ -63,5 +81,5 @@ export function useOrderSSE() {
       abortControllerRef.current?.abort();
       clearTimeout(reconnectTimeoutRef.current as NodeJS.Timeout);
     };
-  }, [queryClient]);
+  }, [queryClient, userData?.id]);
 }
